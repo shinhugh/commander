6dd286b1b31a7ca595a009fcb6567788038c1671
Hugh Shin <hughbee135@gmail.com>
2023-08-04 00:38:19 -0700

    Rename directories

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mvn -f /home/dev/Documents/pixels/backend/pom.xml clean:clean && \
-mvn -f /home/dev/Documents/pixels/backend/pom.xml compiler:compile && \
-mvn -f /home/dev/Documents/pixels/backend/pom.xml war:exploded && \
+mvn -f /home/dev/Documents/pixels/server/pom.xml clean:clean && \
+mvn -f /home/dev/Documents/pixels/server/pom.xml compiler:compile && \
+mvn -f /home/dev/Documents/pixels/server/pom.xml war:exploded && \
 sudo rm -rf /var/lib/tomcat10/webapps/ROOT && \
-sudo cp -r /home/dev/Documents/pixels/backend/target/pixels-1.0/ /var/lib/tomcat10/webapps/ROOT
+sudo cp -r /home/dev/Documents/pixels/server/target/pixels-1.0/ /var/lib/tomcat10/webapps/ROOT
